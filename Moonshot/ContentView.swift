69e78a8229542d9f535065f255b21b45c9da9459
JulyKikuAkita <b92701105@gmail.com>
2021-10-11 18:09:23 -0700

    demo decode json

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,83 +7,36 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new custom text")
-        self.text = text
-    }
-}
-
-struct DogeImageView: View {
-    var body: some View {
-        VStack{
-            GeometryReader { geo in
-                List{
-
-                    Image("fatty1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: geo.size.width)
-                    Image("fatty2")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: geo.size.width)
-                }
-            }
-        }
-    }
-}
-
 struct ContentView: View {
+    var message = "decode fail"
     var body: some View {
-        NavigationView {
-            VStack {
-                NavigationLink(destination: DogeImageView()) {
-                    Text("Meet my Doge")
-                }
-                Image("myFutureNoodles")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: .infinity)
-            }
-            .navigationTitle("Doge")
-        }
-
-
-        //when we add views to a scroll view they get created immediately.
-        ScrollView(.vertical) {
-            VStack(spacing: 10) {
-                ForEach(0..<10) {
-                    CustomText("Row \($0)")
-                        .font(.title)
+        Button("Decode Json") {
+            let input = """
+            {
+                "name": "Noodels",
+                "address": {
+                    "Street": "No 1, Buffalo Ridge"
+                    "City": "New York"
                 }
             }
-            .frame(maxWidth: .infinity)
-        }
-
-        // list gerate the row only when scroll to the row
-        List{
-            ForEach(0..<10) {
-                CustomText("List row \($0)")
-                    .font(.title)
+            """
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
             }
-        }
-
-        NavigationView {
-            List(0..<100) {row in
-                NavigationLink(destination: Text("Detail \(row)")) {
-                    Text("Row \(row)")
-                }
-            }
-            .navigationTitle("SwiftUI")
         }
     }
+}
+
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
 }
 
 struct ContentView_Previews: PreviewProvider {
